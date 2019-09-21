4f9423bcb530fb6889e2df0dadf01072d870df0d
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-09-21 11:42:46 +0530

    Update Nprime.java

--- Nprime.java
+++ Nprime.java
@@ -10,22 +10,20 @@
   					int i,j,n;
   					Scanner np = new Scanner(System.in);
   					System.out.println("Enter Number Range limit");
-            n = np.nextInt();
+                                        n = np.nextInt();
   					System.out.println("Non prime numbers in range 1 - " + n + " are :");
   					for(i=1;i<=n;i++)
   					{    
   						 int m = i/2;
   						for(j = 2;j<=m;++j)
   						   {
-  						   	  if(i%j != 0 )
-  						       {
-  						       	continue;
-  						       }
-  						      else 
-  						       { 
-  						       	System.out.println(i);
-  						       	break;
-  						        }
+  						   	  if(i%j != 0 )continue;
+  						       
+  						         else 
+  						          { 
+  						           System.out.println(i);
+  						           break;
+  						           }
   						 }//end of inner loop
   				  }//end of outer loop
   				}//end of main
