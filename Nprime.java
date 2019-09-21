e2da7fe746f921c6ac2a2250ce2562c97030e9d9
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-09-21 11:44:48 +0530

    Update Nprime.java

--- Nprime.java
+++ Nprime.java
@@ -5,7 +5,7 @@
   import java.util.*;
   public  class  Nprime
   		{
-  			public static void main(String[] args)
+  			public static void main(String args[])
   				{
   					int i,j,n;
   					Scanner np = new Scanner(System.in);
