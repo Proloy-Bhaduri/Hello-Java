2653b8a408dae70bca4c76f06775fd00317eefe3
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-09-21 10:45:22 +0530

    Create FirstNnonprime.java

---
+++ FirstNnonprime.java
@@ -0,0 +1,32 @@
+/* ASSIGNMENT - 1 : 
+  Q. 4) Write a java program to print the first N non-prime numbers
+
+   Solution Made by Proloy Bhaduri
+ /*Solution: */ 
+  import java.util.*;
+  public  class  Nprime
+  		{
+  			public static void main(String args[])
+  				{
+  					int i=2,j,n;
+  					boolean fl;
+  					Scanner np = new Scanner(System.in);// object creation  style in java
+  					System.out.println("Enter Number Range limit");
+                    n = np.nextInt();
+  					//System.out.println("Non prime numbers in range 1 - " + n + " are :");
+  					while (n!=0)
+  					{     
+  						i++; 
+  						for(j = 2;j<=i/2;++j)
+  						   {
+
+  						   	  if(i%j == 0 )
+  						       {
+                      System.out.println(i);
+  						       	n--;
+  						       	break;
+  						        }
+  						    }//end of inner loop
+  				  }//end of outer loop
+  				}//end of main
+  		} //end of class
