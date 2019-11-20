cff57fb8f9714e959f33c8443567682591a9c73c
Proloy Bhaduri <42575314+Proloy-Bhaduri@users.noreply.github.com>
2019-11-20 10:11:48 +0530

    Add files via upload

---
+++ Java/PackageExample.java
@@ -0,0 +1,7 @@
+import demo_pack.*;
+class PackageExample{
+	public static void main(String pb99[]){
+		Demo1 a = new Demo1();
+	   a.msg();
+	}
+}
---
+++ Java/demo_pack/Demo1.java
@@ -0,0 +1,6 @@
+package demo_pack;
+public class Demo1{
+        public void msg(){
+      System.out.println("I'm Demo1 class");
+    }
+}
